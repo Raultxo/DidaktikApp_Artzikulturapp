b26a2dc2226fdea324bc7023413ca233b6e6d1ac
Raultxo <raul6114@gmail.com>
2021-11-29 09:33:54 +0100

    Base

--- app/src/main/java/com/example/didaktikappartzikulturapp/MainActivity.java
+++ app/src/main/java/com/example/didaktikappartzikulturapp/MainActivity.java
@@ -6,22 +6,9 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
-    //comentario
-    // comentario 2
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-// asodkjapsdj
-
-
-//algoasdi
-
-
-
-
-
-
-    //
 }
