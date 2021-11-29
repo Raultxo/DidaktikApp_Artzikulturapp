cc523f799a5db762cdd3f972f4c254dacd55fa3c
Raultxo <raul6114@gmail.com>
2021-11-29 09:29:13 +0100

    Prueba 2

--- app/src/main/java/com/example/didaktikappartzikulturapp/MainActivity.java
+++ app/src/main/java/com/example/didaktikappartzikulturapp/MainActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 public class MainActivity extends AppCompatActivity {
 
     //comentario
+    // comentario 2
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
