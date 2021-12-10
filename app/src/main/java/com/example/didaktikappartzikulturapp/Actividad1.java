7918bcbdb87312f98ef0dcadacb968a192448afd
Kevin <kevinfdezdelanda@gmail.com>
2021-12-10 14:24:33 +0100

    Comienzo actividad1

---
+++ app/src/main/java/com/example/didaktikappartzikulturapp/Actividad1.java
@@ -0,0 +1,48 @@
+package com.example.didaktikappartzikulturapp;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+public class Actividad1 extends AppCompatActivity {
+
+    private Button btnAudio;
+
+    @SuppressLint("ClickableViewAccessibility")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.actividad1);
+        getSupportActionBar().hide();
+
+        View decorView = getWindow().getDecorView();
+        // Hide the status bar.
+        int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION;
+        decorView.setSystemUiVisibility(uiOptions);
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus) {
+            getWindow().getDecorView().setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                    | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                    | View.SYSTEM_UI_FLAG_FULLSCREEN
+                    | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+        }
+    }
+
+    public void reproducirAudio(View v){
+
+    }
+
+}
--- app/src/main/java/com/example/didaktikappartzikulturapp/VentanaDeveloper.java
+++ app/src/main/java/com/example/didaktikappartzikulturapp/VentanaDeveloper.java
@@ -19,20 +19,9 @@ import androidx.fragment.app.FragmentManager;
 
 public class VentanaDeveloper extends AppCompatActivity {
 
-    //dialogos
-    private FragmentManager fragmentManager;
-    private DialogoGrupoNuevo dialogo;
-
-    //lista
-    private ListView lista;
-    final Grupo[] datos = new Grupo[] {
-            new Grupo("Grupo 1",1),
-            new Grupo("Grupo 2",2),
-            new Grupo("Grupo 3",3)
-    };
-
     //botones
     private Button btnVolver;
+    private Button btnAct1, btnAct2, btnAct3, btnAct4, btnAct5, btnAct6, btnAct7, btnAct8;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,17 +35,6 @@ public class VentanaDeveloper extends AppCompatActivity {
         int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION;
         decorView.setSystemUiVisibility(uiOptions);
 
-        // lista de grupos
-        AdaptadorGrupos adaptador=new AdaptadorGrupos(this,datos);
-        lista = (ListView) findViewById(R.id.lstListado);
-        lista.setAdapter(adaptador);
-        lista.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-
-            }
-        });
-
         // vuelve a la ventana principal
         btnVolver = (Button) findViewById(R.id.btnVolver);
         btnVolver.setOnClickListener(new View.OnClickListener() {
@@ -67,21 +45,17 @@ public class VentanaDeveloper extends AppCompatActivity {
                 overridePendingTransition(0,0);
             }
         });
-    }
 
-    class AdaptadorGrupos extends ArrayAdapter <Grupo> {
-        public AdaptadorGrupos(@NonNull Context context, Grupo[] datos) {
-            super(context, R.layout.listitem_grupo, datos);
-        }
-        @NonNull
-        @Override
-        public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
-            LayoutInflater inflater = LayoutInflater.from(getContext());
-            View item = inflater.inflate(R.layout.listitem_grupo, null);
-            TextView lblNombre = (TextView)item.findViewById(R.id.lblNomGrupo);
-            lblNombre.setText(datos[position].getNombre());
-            return (item);
-        }
+        // Botones actividades
+        btnAct1 = (Button) findViewById(R.id.actividad1);
+        btnAct2 = (Button) findViewById(R.id.actividad2);
+        btnAct3 = (Button) findViewById(R.id.actividad3);
+        btnAct4 = (Button) findViewById(R.id.actividad4);
+        btnAct5 = (Button) findViewById(R.id.actividad5);
+        btnAct6 = (Button) findViewById(R.id.actividad6);
+        btnAct7 = (Button) findViewById(R.id.actividad7);
+        btnAct8 = (Button) findViewById(R.id.actividad8);
+
     }
 
     @Override
@@ -96,6 +70,48 @@ public class VentanaDeveloper extends AppCompatActivity {
                     | View.SYSTEM_UI_FLAG_FULLSCREEN
                     | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
             );
+        }
+    }
+
+    public void abrirActividad(View v){
+        if(v.equals(btnAct1)){
+            Intent intent = new Intent(VentanaDeveloper.this, Actividad1.class);
+            startActivity(intent);
+        }
+
+        if(v.equals(btnAct2)){
+            //Intent intent = new Intent(VentanaDeveloper.this, Actividad2.class);
+            //startActivity(intent);
+        }
+
+        if(v.equals(btnAct3)){
+            //Intent intent = new Intent(VentanaDeveloper.this, Actividad3.class);
+            //startActivity(intent);
+        }
+
+        if(v.equals(btnAct4)){
+            //Intent intent = new Intent(VentanaDeveloper.this, Actividad4.class);
+            //startActivity(intent);
+        }
+
+        if(v.equals(btnAct5)){
+            //Intent intent = new Intent(VentanaDeveloper.this, Actividad5.class);
+            //startActivity(intent);
+        }
+
+        if(v.equals(btnAct6)){
+            //Intent intent = new Intent(VentanaDeveloper.this, Actividad6.class);
+            //startActivity(intent);
+        }
+
+        if(v.equals(btnAct7)){
+            //Intent intent = new Intent(VentanaDeveloper.this, Actividad7.class);
+            //startActivity(intent);
+        }
+
+        if(v.equals(btnAct8)){
+            //Intent intent = new Intent(VentanaDeveloper.this, Actividad8.class);
+            //startActivity(intent);
         }
     }
 }
