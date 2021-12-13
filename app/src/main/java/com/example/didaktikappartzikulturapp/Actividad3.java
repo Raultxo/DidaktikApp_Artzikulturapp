e648363945d2b3e1d872b63fe7de20622ef0fb85
Raultxo <raul6114@gmail.com>
2021-12-13 12:58:40 +0100

    Texto que aparece letra a letra

--- app/src/main/java/com/example/didaktikappartzikulturapp/Actividad3.java
+++ app/src/main/java/com/example/didaktikappartzikulturapp/Actividad3.java
@@ -3,6 +3,8 @@ package com.example.didaktikappartzikulturapp;
 import android.annotation.SuppressLint;
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.os.SystemClock;
 import android.util.Log;
 import android.view.View;
@@ -12,11 +14,13 @@ import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class Actividad3 extends AppCompatActivity {
 
     private String texto = "Herri polit honetako biztanleek, alegia, zuek, produktuak Erdi Aroan nola saltzen eta trukatzen zituzten urtero irudikatzen duzue. Dakizuenez, azoka bitxi hau udaren amaieran ospatzen da, baina badakizue zergatik momentu horretan? Uda amaiera, uzta biltzeko garaia izan ohi zen, hortaz, hainbat zonaldetako produktuen elkartrukea egiten zuten Artziniegan bertan.\n" +
             "Artziniegako kale guztiak apaintzen dituzte, iraganera bost mende bidaiatzeko. Gainera, kaleetan zehar artisautza eta gastronomiako erakustoki desberdinak daude, Erdi Arora hegaz eramaten gaituztenak. Horrez gain, kaleetatik pertsonaia desberdinak topa daitezke: zaldunak, nobleak, bufoiak, malabaristak, artisauak, elizgizonak, titiriteroak, trobadoreak, dontzeilak, perkusionistak, zingaroak, juglareak, eta abar. Zuei zein pertsonaietaz mozorratzea gustatzen zaizue? \n";
-    private String[] textoArray = texto.split("");
     private Button btnVolver;
     private ImageButton btnAudio;
     private TextView textView;
@@ -48,11 +52,7 @@ public class Actividad3 extends AppCompatActivity {
         btnAudio.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
-                for(int i = 0; i < textoArray.length; i++) {
-                    textView.append(textoArray[i]);
-                }
-
+                reproducirAudio();
             }
         });
     }
@@ -71,9 +71,44 @@ public class Actividad3 extends AppCompatActivity {
         }
     }
 
-    public void reproducirAudio(View v){
+    public void reproducirAudio(){
         MediaPlayer mp = MediaPlayer.create(this, R.raw.audio3);
         mp.start();
+        textView.setText("");
+        setText(texto);
+
+    }
+
+    public void setText(final String s)
+    {
+        final int[] i = new int[1];
+        i[0] = 0;
+        final int length = s.length();
+
+        final Handler handler = new Handler()
+        {
+            @Override
+            public void handleMessage(Message msg) {
+                super.handleMessage(msg);
+                char c= s.charAt(i[0]);
+                Log.d("Strange",""+c);
+                textView.append(String.valueOf(c));
+                i[0]++;
+            }
+        };
+
+        final Timer timer = new Timer();
+        TimerTask taskEverySplitSecond = new TimerTask() {
+            @Override
+            public void run() {
+                handler.sendEmptyMessage(0);
+                if (i[0] == length - 1) {
+                    timer.cancel();
+                }
+            }
+        };
+        timer.schedule(taskEverySplitSecond, 1, 65);
+
     }
 
 }
