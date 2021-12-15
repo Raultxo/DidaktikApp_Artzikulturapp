642df2e5057d057b3e2a42f84db6137fd3ec8d9c
Elena <elenafesional@gmail.com>
2021-12-15 10:02:11 +0100

    actividad2, falta el fragment y el audio

--- app/src/main/java/com/example/didaktikappartzikulturapp/Actividad2.java
+++ app/src/main/java/com/example/didaktikappartzikulturapp/Actividad2.java
@@ -5,6 +5,9 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RelativeLayout;
+import android.widget.TableLayout;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -38,45 +41,86 @@ public class Actividad2 extends AppCompatActivity {
         respuestas=new HashMap<EditText,String>();
         EditText aux=(EditText) findViewById(R.id.resp1);
         respuestas.put(aux,"santutegi");
+        aux.setText("santutegi");
+
         aux=(EditText) findViewById(R.id.resp2);
         respuestas.put(aux,"alboan");
+        aux.setText("alboan");
+
         aux=(EditText) findViewById(R.id.resp3);
         respuestas.put(aux,"artzain");
+        aux.setText("artzain");
+
         aux=(EditText) findViewById(R.id.resp4);
-        respuestas.put(aux,"Birjina");
+        respuestas.put(aux,"birjina");
+        aux.setText("birjina");
+
         aux=(EditText) findViewById(R.id.resp5);
         respuestas.put(aux,"tenpluaren");
+        aux.setText("tenpluaren");
+
         aux=(EditText) findViewById(R.id.resp6);
         respuestas.put(aux,"mende");
+        aux.setText("mende");
+
         aux=(EditText) findViewById(R.id.resp7);
         respuestas.put(aux,"eraikin");
+        aux.setText("eraikin");
+
         aux=(EditText) findViewById(R.id.resp8);
         respuestas.put(aux,"aterpe");
+        aux.setText("aterpe");
+
         aux=(EditText) findViewById(R.id.resp9);
         respuestas.put(aux,"lau");
+        aux.setText("lau");
+
         aux=(EditText) findViewById(R.id.resp10);
         respuestas.put(aux,"eskultura");
+        aux.setText("eskultura");
     }
 
 
     public void corregir()
     {
         Iterator<EditText> it = respuestas.keySet().iterator();
-
+        int correctas=0;
         while(it.hasNext()){
             EditText edit = it.next();
             String resp = respuestas.get(edit);
-            String texto = edit.getText().toString();
+            String texto = edit.getText().toString().toLowerCase();
 
             if(texto.equals(resp))
             {
                 edit.setTextColor(getResources().getColor(R.color.correcto));  //FF48B54D --> verde
                 edit.setEnabled(false);
+                correctas++;
             }
             else
             {
                 edit.setText("");
             }
+        }
+
+        if(correctas==10)
+        {
+            //ocultar texto
+            RelativeLayout rel=(RelativeLayout) findViewById(R.id.textoLayout);
+            rel.setVisibility(View.GONE);
+
+            Button btn=(Button) findViewById(R.id.btnCorregir);
+            btn.setVisibility(View.GONE);
+            btn=(Button) findViewById(R.id.btnAudio);
+            btn.setVisibility(View.GONE);
+
+            TableLayout tabla=(TableLayout) findViewById(R.id.tabla);
+            tabla.setVisibility(View.GONE);
+
+            //marcar la actividad como hecha en la info del grupo
+
+            //felicidades por haber terminado, etc, etc
+                //fragment_fin_acti2
+
         }
     }
 }
